0e68ac0c57588396ee061f4187a7fd5fed4eabed
aacister <aacister@gmail.com>
2016-04-05 12:30:39 -0400

    google oauth callback url change
    Changed google oauth callback to point to heroku site

--- config/auth.js
+++ config/auth.js
@@ -4,7 +4,8 @@ module.exports = {
     'googleAuth' : {
         'clientID'      : '831646395270-n9ub4g0vk1lojcbo8h27l0tj31efr1re.apps.googleusercontent.com',
         'clientSecret'  : 'uaNKsO7zwhCSoxmRMPxJ4FWx',
-        'callbackURL'   : 'http://localhost:3000/auth/google/callback'
+        'callbackURL' :'https://quiet-forest-46848.herokuapp.com/auth/google/callback'
+    //    'callbackURL'   : 'http://localhost:3000/auth/google/callback'
     }
 
 };
